27fb99592dda7400e03dbae6dd4b3600b802bae0
Sarah Ju <sarahju@Sarahs-MacBook-Air.local>
2022-04-14 22:33:42 -0700

    upload vr project to git

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+[RequireComponent(typeof(Animator))]
+
+public class Hand : MonoBehaviour
+{
+    public float speed;
+    Animator Animator;
+    SkinnedMeshRenderer mesh;
+
+    // valem
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    //
+
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInitialize();
+        // animator = GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        // always work even if there is no controller
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            // trigger
+            if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+            {
+                handAnimator.SetFloat("Trigger", triggerValue);
+            }
+            else
+            {
+                handAnimator.SetFloat("Trigger", 0);
+            }
+
+            // grip
+            if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            {
+                handAnimator.SetFloat("Grip", gripValue);
+            }
+            else
+            {
+                handAnimator.SetFloat("Grip", 0);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+            UpdateHandAnimation();
+        }
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            // animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            // animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+        
+    }
+
+    public void ToggleVisibility()
+    {
+        mesh.enabled = !mesh.enabled;
+    }
+}
