04a84c2713cb0e7de16441431dce39411b544b24
chaoranji <103224093+chaoranji@users.noreply.github.com>
2022-04-22 02:28:11 -0700

    Smooth movement

--- Assets/Scripts/followToCamera.cs
+++ Assets/Scripts/followToCamera.cs
@@ -6,13 +6,21 @@ public class followToCamera : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 10.0f;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void start()
     {
         transform.position = target.position + Vector3.up * offset.y 
             + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
             + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp ( transform.position, target.position + Vector3.up * offset.y 
+            + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
+            + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z, Time.deltaTime * moveSpeed);
         
         transform.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
         transform.rotation = target.transform.rotation * Quaternion.Euler(0, 180, 0);
