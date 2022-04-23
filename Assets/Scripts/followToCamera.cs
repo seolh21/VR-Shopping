c906645a084bd76e54df99b5d821378a7cfc2a7b
chaoranji <103224093+chaoranji@users.noreply.github.com>
2022-04-23 01:08:34 -0700

    Update followToCamera.cs

--- Assets/Scripts/followToCamera.cs
+++ Assets/Scripts/followToCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class followToCamera : MonoBehaviour
 {
@@ -8,9 +9,14 @@ public class followToCamera : MonoBehaviour
     public Vector3 offset;
     public float moveSpeed = 10.0f;
     public float rotateSpeed = 10.0f;
+    private int flag = 0;
+    ActionBasedController controller;
+
 
     void start()
     {
+        controller = GetComponent<ActionBasedController>();
+
         transform.position = target.position + Vector3.up * offset.y 
             + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
             + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;
@@ -18,11 +24,31 @@ public class followToCamera : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(controller.selectAction.action.ReadValue<float>() > 0.5)
+        {
+            RaycastHit hit;
+            // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = new Ray(controller.transform.position, controller.transform.forward);
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.collider.gameObject.name == this.name)
+                {
+                   
+                    flag = 1-flag;
+                    
+                }
+            }
+        }
+        Debug.Log(flag);
+
+        if(flag == 0)
+        {
         transform.position = Vector3.Lerp ( transform.position, target.position + Vector3.up * offset.y 
             + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
             + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z, Time.deltaTime * moveSpeed);
         
         transform.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
         transform.rotation = target.transform.rotation * Quaternion.Euler(0, 180, 0);
+        }
     }
 }
